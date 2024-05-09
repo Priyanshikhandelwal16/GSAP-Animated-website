041641bb92e6900cb93215536668fd37b87d3550
Priyanshi Khandelwal <161630817+Priyanshikhandelwal16@users.noreply.github.com>
2024-05-09 21:32:18 +0530

    animated-website

---
+++ script.js
@@ -0,0 +1,91 @@
+function page1Animation(){
+    var tl = gsap.timeline()
+
+    tl.from("nav h1, nav h4, nav button",{
+        y: -40,
+        duration:0.7,
+        delay:0.5,
+        opacity:0,
+        stagger:0.15
+    })
+    tl.from(".center-part1 h1",{
+        x:-200,
+        opacity:0,
+        duration:0.5
+    },"-=0.3")
+    tl.from(".center-part1 p",{
+        x:-100,
+        opacity:0,
+        duration:0.4
+    })
+    tl.from(".center-part1 button",{
+        opacity:0,
+        duration:0.4
+    })
+    tl.from(".center-part2 img",{
+        opacity:0,
+        duration:0.5,
+        x:200
+    },"-=0.7")
+    tl.from(".section1bottom img",{
+        opacity:0,
+        y:30,
+        stagger:0.15,
+        duration:0.6
+    })
+}
+
+
+
+function page2Animation(){
+    var tl2 = gsap.timeline({
+        scrollTrigger:{
+            trigger:".section2",
+            scroller:"body",
+            start:"top 50%",
+            end:"top -75%",
+            scrub:2,
+        }
+    })
+    
+    tl2.from(".services",{
+        y:30,
+        opacity:0,
+        duration:0.5
+    })
+    // line1
+    
+    tl2.from(".elem.line1.left",{
+           x:-300,
+           opacity:0,
+           duration:1,
+    },"anim1");
+    
+    tl2.from(".elem.black.line1.right",{
+        x:300,
+        opacity:0,
+        duration:1
+    },"anim1");
+    
+    // line2
+    
+    tl2.from("#email",{
+        x:90,
+        opacity:0,
+        duration:1,
+    },"anim2");  
+
+
+    tl2.from("#social",{
+        x:-90,
+        opacity:0,
+        duration:1,
+    },"anim2");
+    
+
+     
+}
+
+page1Animation();
+
+page2Animation();
